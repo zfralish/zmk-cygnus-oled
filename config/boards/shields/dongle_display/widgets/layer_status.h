0dc838425c28cf030318584bc7d06606c98c27d3
rain2813 <149554389+rain2813@users.noreply.github.com>
2024-03-25 02:13:57 +0800

    Update layer_status.h

--- config/boards/shields/dongle_display/widgets/layer_status.h
+++ config/boards/shields/dongle_display/widgets/layer_status.h
@@ -8,11 +8,14 @@
 
 #include <lvgl.h>
 #include <zephyr/kernel.h>
+#include <dt-bindings/zmk/modifiers.h>
 
-struct zmk_widget_layer_status {
+#define SIZE_SYMBOLS 14 // 14 x 14 pixel
+
+struct zmk_widget_modifiers {
     sys_snode_t node;
     lv_obj_t *obj;
 };
 
-int zmk_widget_layer_status_init(struct zmk_widget_layer_status *widget, lv_obj_t *parent);
-lv_obj_t *zmk_widget_layer_status_obj(struct zmk_widget_layer_status *widget);
+int zmk_widget_modifiers_init(struct zmk_widget_modifiers *widget, lv_obj_t *parent);
+lv_obj_t *zmk_widget_modifiers_obj(struct zmk_widget_modifiers *widget);
